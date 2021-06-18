f2292d7250cd0b30b218a8b791d68fb0fe50acc9
smrpn <samarpan_d@pp.iitr.ac.in>
2021-06-18 07:29:05 +0530

    ft: initial commit of the Casbin grpc service
    Signed-off-by: smrpn <samarpan_d@pp.iitr.ac.in>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use casbin::casbin_server::CasbinServer;
+
+//use crate::casbin::casbin_server::Casbin;
+
+use tonic::transport::Server;
+
+pub mod casbin {
+    tonic::include_proto!("proto");
+}
+
+#[derive(Debug, Default)]
+pub struct CasbinStruct {}
+
+pub trait CasbinTrait {}
+
+#[tonic::async_trait]
+impl CasbinTrait for CasbinStruct {}
+
+// Runtime to run the server
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse()?;
+    let casbinserver = CasbinStruct::default();
+    println!("Start gRPC server...");
+    Server::builder()
+        .add_service(CasbinServer::new(casbinserver))
+        .serve(addr)
+        .await?;
 }
