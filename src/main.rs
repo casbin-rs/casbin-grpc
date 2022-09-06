3290f5402201d79ff0ef71c4fd113a5b5c71cc91
SiddheshKanawade <siddhesh.kanawade@iitgn.ac.in>
2022-09-07 02:48:51 +0530

    feat: implement hashmap datastructure

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,36 @@ use std::collections::HashMap;
 pub mod casbin_proto {
     tonic::include_proto!("proto");
 }
+pub mod datastructure;
 pub mod proto;
 pub mod server;
 use casbin::{Adapter, CachedEnforcer};
-
 use futures::lock::Mutex;
+use std::cell::RefCell;
 use std::sync::Arc;
 
+use crate::datastructure::hashtable::HashTable;
 // Arc is used to share data betweeen the threads, threads in rust?
 
 #[derive(Default)]
 pub struct CasbinGRPC {
     enforcer_map: HashMap<i32, Arc<Mutex<CachedEnforcer>>>,
-    adapter_map: HashMap<i32, Box<dyn Adapter>>,
+    // use RefCell and check if thread safe, failed
+    // use mutex+ arc
+    // something similar to RefCell but thread safe
+    // drop mutex guard
+    // Atomic
+    // RefCell with Arc, failed
+    // Arc+Rwlock
+    // HashBrown
+    adapter_map: HashMap<i32, Arc<Mutex<Box<dyn Adapter>>>>,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr: String = "[::1]:50051".parse().unwrap();
     let casbin = CasbinGRPC::default();
+    // let mut hash = HashTable::<i32, Box<dyn Adapter>>::new();
 
     println!("Server listening on: {}", addr);
     //Server::builder()
--- src/server/enforcer.rs
+++ src/server/enforcer.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{borrow::BorrowMut, cell::RefCell, collections::HashMap, ops::Deref};
 
 use crate::server::abac;
 use crate::CasbinGRPC;
@@ -20,15 +20,21 @@ impl CasbinGRPC {
         Ok(wrapped_cachedenforcer)
         // self.enforcer_map.get(&handle).ok_or("No enforcer found")
     }
-    pub fn get_adapter(&self, handle: i32) -> Result<&Box<dyn Adapter>, &str> {
-        self.adapter_map.get(&handle).ok_or("No adapter found")
+    pub async fn get_adapter(&self, handle: i32) -> Result<Arc<Mutex<Box<dyn Adapter>>>, &str> {
+        // let wrapped_adapter = self.adapter_map.get(&handle).unwrap().to_owned();
+        // let x = self.adapter_map.get(&handle).unwrap().to_owned();
+        // Ok(wrapped_adapter)
+        let x = self.adapter_map.get(&handle).unwrap().to_owned();
+        let y = self.adapter_map.get_key_value(&handle);
+        Ok(x)
+        // self.adapter_map.get(&handle).ok_or("No adapter found")
     }
     pub fn add_enforcer(&mut self, e: Arc<Mutex<CachedEnforcer>>) -> i32 {
         let cnt: i32 = self.enforcer_map.len() as i32;
         self.enforcer_map.insert(cnt, e);
         cnt
     }
-    pub fn add_adapter(&mut self, a: Box<dyn Adapter>) -> i32 {
+    pub fn add_adapter(&mut self, a: Arc<Mutex<Box<dyn Adapter>>>) -> i32 {
         let cnt: i32 = self.adapter_map.len() as i32;
         self.adapter_map.insert(cnt, a);
         cnt
