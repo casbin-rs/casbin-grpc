57e12c4fa7416999213aee7ae320b891d346d54d
imp2002 <imp07@qq.com>
2023-01-17 23:01:14 +0800

    fix: fix server start

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,13 @@ use std::collections::HashMap;
 pub mod casbin_proto {
     tonic::include_proto!("proto");
 }
-pub mod proto;
 pub mod server;
 use casbin::{Adapter, CachedEnforcer};
 
 use futures::lock::Mutex;
 use std::sync::Arc;
+use tonic::transport::Server;
+use crate::casbin_proto::casbin_server::CasbinServer;
 
 // Arc is used to share data betweeen the threads, threads in rust?
 
@@ -19,14 +20,14 @@ pub struct CasbinGRPC {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr: String = "[::1]:50051".parse().unwrap();
+    let addr = "[::1]:50051".parse().unwrap();
     let casbin = CasbinGRPC::default();
 
     println!("Server listening on: {}", addr);
-    //Server::builder()
-    //    .add_service(CasbinServer::new(casbin))
-    //    .serve(addr)
-    //    .await?;
+    Server::builder()
+       .add_service(CasbinServer::new(casbin))
+       .serve(addr)
+       .await?;
 
     Ok(())
 }
